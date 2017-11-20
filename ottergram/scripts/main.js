61a0d0658c9c875a4c7537e6f1f13c65695b07da
AnnGabr <annabaks1997@gmail.com>
2017-11-20 11:09:22 +0300

    variables declared

---
+++ ottergram/scripts/main.js
@@ -0,0 +1,9 @@
+"use strict"
+;(function() {
+  var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+  var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+  var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+
+
+})
