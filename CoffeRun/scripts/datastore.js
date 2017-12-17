33946ed3969410295d3324536b26b0976b51c540
AnnGabr <annabaks1997@gmail.com>
2017-12-18 00:00:31 +0300

    module completed

--- CoffeRun/scripts/datastore.js
+++ CoffeRun/scripts/datastore.js
@@ -1,10 +1,27 @@
 ;(function(window){
+  "use strict"
   var App = window.App || {};
 
   function DataStore(){
-    console.log("running ds");
+    this.data = {};
   }
+
+  DataStore.prototype.add = function (key, val) {
+    this.data[key] = val;
+  };
+
+  DataStore.prototype.remove = function (key) {
+    delete this.data[key];
+  };
+
+  DataStore.prototype.get = function (key) {
+    return this.data[key];
+  };
+
+  DataStore.prototype.getAll = function () {
+    return this.data;
+  };
 
   App.DataStore = DataStore;
   window.App = App;
-})
+})(window);
