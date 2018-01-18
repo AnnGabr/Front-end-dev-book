bab147b160c68529c8ddda4f54a6cb85b7d2a4bc
AnnGabr <annabaks1997@gmail.com>
2018-01-18 19:29:43 +0300

    coffeiine range handler added

---
+++ CoffeRun/scripts/rangeHandler.js
@@ -0,0 +1,36 @@
+;(function(window){
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function RangeHandler(selector){
+      if(!selector){
+        throw new Error('No selector provided');
+      }
+
+      this.$rangeElement = $(selector);
+      if(this.$rangeElement.length !== 1){
+        throw new Error('No range element or more then one found with selector: ' + selector);
+      }
+  }
+
+  RangeHandler.prototype.addChangeHandler = function(fn){
+    this.$rangeElement.on('change', function(){
+      var newValue = this.elements[0].value;
+      switch(newValue){
+        case '0': console.log('0');
+          break;
+        case '1':
+          break;
+        case '2':
+          break;
+        case '3':
+          break;
+      }
+      if(fn)
+        fn(newValue);
+    });
+  }
+
+  App.RangeHandler = RangeHandler;
+  window.App = App;
+})(window);
