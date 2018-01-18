8906f4e09387cd72b8ee0f03ac25f840d5329971
AnnGabr <annabaks1997@gmail.com>
2018-01-18 19:30:09 +0300

    coffeine range handler added to window

--- CoffeRun/scripts/main.js
+++ CoffeRun/scripts/main.js
@@ -1,15 +1,19 @@
 ;(function(window){
   "use strict"
   var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var COFFEINE_RANGE_SELECTOR = '[data-coffeine-level="strength"]';
 
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  var RangeHandler = App.RangeHandler;
 
   var myTruck = new Truck('ncc-1701', new DataStore());
   var formHandler = new FormHandler(FORM_SELECTOR);
+  var coffeineRangeHandler = new RangeHandler(COFFEINE_RANGE_SELECTOR);
 
   formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  coffeineRangeHandler.addChangeHandler();
   console.log(formHandler);
 })(window);
